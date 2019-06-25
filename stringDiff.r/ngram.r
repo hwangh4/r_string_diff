d22ad66c2678ddbcce3945464d43fb843432127d
Scarlett <hwang.scarlett@gmail.com>
2019-06-25 14:52:34 -0700

    freq list works

--- stringDiff.r/ngram.r
+++ stringDiff.r/ngram.r
@@ -1,5 +1,10 @@
+## Author: Scarlett Hwang
+## Eviction OCR Research 
+## June 25th, 2019
+
 library(readr)
 
+## read in files (TBA)
 file <- read_file("text1.txt")
 file_array <- c()
 file_array <- append(file_array, file)
@@ -9,25 +14,38 @@ file_array <- append(file_array, file)
 ## params: sentence - the sentence we want to analyze
 ##         num - number of n-gram (2 for bigram)
 word_ngram <- function(sentence, num) {
+  # remove new lines
   sentence <- stringr::str_replace(sentence, '\n', ' ')
+  # tokenize words
   text <- strsplit(sentence, ' ') %>% Reduce(c, .)
   ngram <- c()
+
   for (x in 1:length(text)) {
     if (nchar(text[x]) < 2)
       next
     splitted <- c(strsplit(text[x], ''))[[1]]
-    # print(splitted)
     for (letter in 1:(length(splitted)-1)) {
       ngram <- append(ngram, paste0(splitted[seq(letter, letter+num-1)], collapse = ""))
     }
   }
-  return(c(ngram))
+  freq_list(c(ngram))
 }
-
-print(word_ngram(file, 2))
 
 ## make frequency list
-## recieve every occurance from word_ngram function and create a data frame 
-freq_list <- function(word_ngram) {
-  data_frame <- data.frame(...)
+## recieve every occurance from word_ngram function and create a named vector list
+## params: ngram - bigram pairs of passed text file
+freq_list <- function(ngram) {
+  v <- NULL
+  # set start number (0) to all unique bigrams
+  for (i in 1:length(ngram)) {
+      v[ngram[i]] <- 0
+  }
+  # increment counts of a unique bigram
+  for (i in 1:length(ngram)) {
+    v[ngram[i]] <- v[ngram[i]] + 1
+  }
+  return(v)
 }
+
+# execute
+word_ngram(file, 2)
