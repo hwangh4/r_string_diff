7879bdded3fe769da5e6c5678ce34133b5339f40
Scarlett <hwang.scarlett@gmail.com>
2019-07-16 13:52:54 -0700

    new codes

--- stringDiff.r/ngram.r
+++ stringDiff.r/ngram.r
@@ -3,11 +3,19 @@
 ## June 25th, 2019
 
 library(readr)
+library(dplyr)
 
-## read in files (TBA)
-file <- read_file("text1.txt")
-file_array <- c()
-file_array <- append(file_array, file)
+## main 1, read in files 
+# dir <- list.files(pattern="*.txt")
+# v <- NULL
+# for (i in 1:length(dir)) {
+#   word_ngram(read_file(dir[i]), 2)
+# }
+
+## main 2, compare two files' probabilities
+orig_table <- word_ngram(read_file("text1.txt"), 2)
+ocr_table <- word_ngram(read_file("text2.txt"), 2) #HERE compare needed
+
 
 ## word n-gram 
 ## passes every occurance to chart maker function
@@ -23,7 +31,7 @@ word_ngram <- function(sentence, num) {
   for (x in 1:length(text)) {
     if (nchar(text[x]) < 2)
       next
-    splitted <- c(strsplit(text[x], ''))[[1]]
+    splitted <- c(strsplit(text[x], ''))[[1]] #substring instead
     for (letter in 1:(length(splitted)-1)) {
       ngram <- append(ngram, paste0(splitted[seq(letter, letter+num-1)], collapse = ""))
     }
@@ -35,17 +43,9 @@ word_ngram <- function(sentence, num) {
 ## recieve every occurance from word_ngram function and create a named vector list
 ## params: ngram - bigram pairs of passed text file
 freq_list <- function(ngram) {
-  v <- NULL
   # set start number (0) to all unique bigrams
-  for (i in 1:length(ngram)) {
-      v[ngram[i]] <- 0
-  }
-  # increment counts of a unique bigram
-  for (i in 1:length(ngram)) {
-    v[ngram[i]] <- v[ngram[i]] + 1
-  }
-  return(v)
+  return(table(ngram))
+  #print(prop.table(table(ngram)))
 }
 
-# execute
-word_ngram(file, 2)
+
